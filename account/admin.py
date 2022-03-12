85f221bad4d25d3d746368f77c0b08cceaa1715f
izumi <izumi@example.com>
2022-03-12 22:00:50 +0900

    AbstractUserへの変更

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
+
+# class AccountAdmin(UserAdmin):
+#    model = Account
+#    fieldsets = UserAdmin.fieldsets
 
 admin.site.register(Account)
--- account/models.py
+++ account/models.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Account(models.Model):
-    name = models.CharField(max_length=30)
+class Account(AbstractUser):
+    #name = models.CharField(max_length=30)
     email = models.EmailField()
     password = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.name
+        return self.username
